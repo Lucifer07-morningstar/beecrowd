3cdf9fcc20aa0ea9c76710f04e01733b464619c0
Lucifer07-morningstar <96464737+Lucifer07-morningstar@users.noreply.github.com>
2022-11-10 13:17:37 -0400

    Create 1071.c

---
+++ 1071.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,y,temp,sum = 0,i;
+    scanf("%d %d",&x,&y);
+
+    if(x>y)
+    {
+        temp = x;
+        x = y;
+        y = temp;
+    }
+
+    for(i=x+1;i<y;i++)
+    {
+        if(i%2!=0)
+        {
+            sum+=i;
+        }
+
+    }
+    printf("%d\n",sum);
+
+}
